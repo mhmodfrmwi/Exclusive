785c312bf455eb6dd49370e64b6c924f9731a239
mhmodfrmwi <mahmoudframawi98@gmail.com>
2024-07-30 12:35:24 +0300

    reolve responsive

--- src/app/Navbar/Page.js
+++ src/app/Navbar/Page.js
@@ -29,13 +29,13 @@ export default function Navbar() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
   return (
-    <nav className={`bg-white w-full border-b min-[769px]:border-0 shadow p-2`}>
-      <div className="items-center px-4 max-w-screen-xl mx-auto min-[769px]:flex min-[769px]:px-8">
-        <div className="flex items-center justify-between py-3 min-[769px]:py-5 min-[769px]:block">
+    <nav className={`bg-white w-full border-b min-[790px]:border-0 shadow p-2`}>
+      <div className="items-center px-4 max-w-screen-xl mx-auto min-[790px]:flex min-[790px]:px-8">
+        <div className="flex items-center justify-between py-3 min-[790px]:py-5 min-[790px]:block">
           <Link href="/">
             <h1 className="text-3xl font-bold text-gray-950">Exclusive</h1>
           </Link>
-          <div className="min-[769px]:hidden">
+          <div className="min-[790px]:hidden">
             <button
               className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
               onClick={() => setState(!state)}
@@ -45,11 +45,11 @@ export default function Navbar() {
           </div>
         </div>
         <div
-          className={`flex-1 justify-self-center pb-3 mt-8 min-[769px]:block min-[769px]:pb-0 md:mt-0 ${
+          className={`flex-1 justify-self-center pb-3 mt-8 min-[790px]:block min-[790px]:pb-0 md:mt-0 ${
             state ? "block" : "hidden"
           }`}
         >
-          <ul className="justify-center items-center space-y-8 min-[769px]:flex min-[769px]:space-x-6 min-[769px]:space-y-0">
+          <ul className="justify-center items-center space-y-8 min-[790px]:flex min-[790px]:space-x-6 min-[790px]:space-y-0">
             {menus.map((item, idx) => (
               <li key={idx} className="text-gray-600 hover:text-indigo-600">
                 <Link href={item.path}>{item.title}</Link>
